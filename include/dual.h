e263d146b128e59a2c1b75ded8b71a9fee8e060b
yhz <35647076+miaonima@users.noreply.github.com>
2023-12-13 18:04:23 +0800

    design dual vector storage

--- include/dual.h
+++ include/dual.h
@@ -1,49 +1,69 @@
 #include <vector>
+#include <map>
 
 // a container that stores deduplicated float vectors of specified dimensions into a continuous memory space.
-// an element of value 1.0f will be prepended into the vector, and specified number of zeros will be appended to each input vector
+// specified number of zeros will be appended to each input vector.
+// the container will not accept vectors that are already in the container.
+// The container has a maximum number of vectors it can store. When it is full, the oldest vector will be replaced.
 class DualVertexSet
 {
 public:
-    // create an empty container
-    DualVertexSet(size_t original_dims);
-
-    // specify
-    DualVertexSet(int original_dims, int zero_pad_dims);
+    // construct an empty vector set by specifying the number of zero paddings
+    DualVertexSet(size_t original_dims, size_t max_size);
 
     // number of vectors stored in this container.
-    size_t get_length();
+    size_t size() const;
 
-    // get the number of original dimensions of each vector
-    // note: to read the entire vector, actual_dims+1 elements should be read
-    size_t get_actual_dims();
+    // get the number of original dimensions of full vector
+    size_t full_original_dims() const;
 
-    // get the number of actual dimensions of each vector
-    size_t get_actual_dims();
+    // get the number of original dimensions of full vector
+    size_t full_padded_dims() const;
+
+    // get the number of actual dimensions of short vector
+    size_t short_actual_dims() const;
+
+    // get the number of actual dimensions of short vector
+    size_t short_padded_dims() const;
 
     // add a double vector or float vector to the end
-    // returns the address of the added vector
+    // returns the index of the added vector
     // if the vector is not added, returns nullptr
-    // FIXME: how to accept single vector as well?
-    float *insert(const std::vector<float>);
+    size_t insert(const std::vector<float>);
 
     // get the address of the pos-th vector
     // no edits should be made through this pointer
-    const float *get_address(size_t pos);
+    const float *get_full_dual_ptr(size_t pos) const;
+
+    const float *get_short_dual_ptr(size_t pos) const;
 
 private:
-    // the dimension of the input vector
-    size_t original_dims;
+    size_t full_original_dims; // the dimension of the full dual vector
+    size_t full_padded_dims;      // original plus the number of zero-padding for full dual vectors
+    size_t short_original_dims;   // the dimension of the short dual vector
+    size_t short_padded_dims;   // original plus the number of zero-padding for the short dual vector
+    size_t current_size;       // number of vectors currently stored
 
-    // the number of zero paddings at the end of the vector
-    size_t zero_pad_dims;
+    size_t max_size; // maximum number of vectors this container will accept.
 
-    // the dimension of the vector in duals
-    size_t actual_dims;
+    std::vector<float> full_duals; // the full dual vectors
+    std::vector<float> short_duals; // the truncated version of the duals
 
-    // number of vectors currently stored
-    size_t current_length;
+    // // internal class that is used to de-duplicate a float vector using hashes
+    // class Deduplicate
+    // {
+    // public:
+    //     // hash function
+    //     static unsigned int hash(const std::vector<float> &vec);
 
-    // the actual container that stores the vertex
-    std::vector<float> duals;
+    // private:
+    //     // insert an hash-index pair into the hash map
+    //     bool insert(size_t index);
+
+    //     // remove the entry of the index from the hash map
+    //     void removeIndex(size_t index);
+
+    //     // the hash of each vector corresponding to index
+    //     std::multimap<unsigned int, size_t> indexHash;
+    // };
 };
